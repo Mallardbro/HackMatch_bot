3ef5a6fba7477b4e276b5f9135c115ff610348f6
Tim <mallardbro@hotmail.co.uk>
2018-08-29 17:43:11 +0100

    -- Moved grid stuff into Grid class. -- TODO: Move tiles = [] and appending tiles into grid class.

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,34 +1,41 @@
 import cv2
 import numpy as np
+
 from matplotlib import pyplot as plt
 
 # TODO - Change fake violet-pip.jpg to real image template.
 # - Collect screenshots with violet-pip visible
 
-
 class Grid:
     def __init__(self, _tiles):
+
+        # Set row of tiles ('normalise' y by subtracting minimum.)
+        min_y = min([t.iy for t in _tiles])
+        for t in _tiles:
+            t.row = int(round((t.iy - min_y) / 100))
+            t.set_index()
+
+        tiles.sort(key=lambda x: x.index)
         self.tiles = [None] * 70
         for t in _tiles:
             self.tiles[t.index] = t
 
     def get_row(self, num):
-        # Change to mod arithmetic, we want all elements in row, not just valid tiles.
-        # valid = [t for t in self.tiles if t]
-        # return [t for t in valid if t.row == num]
         return self.tiles[num * 7:num * 7 + 7]
 
     def get_column(self, num):
-        # Change to mod arithmetic, we want all elements in column, not just valid tiles.
-        # valid = [t for t in self.tiles if t]
-        # return [t for t in valid if t.col == num]
         return self.tiles[num::7]
 
+    def draw(self):
+        for t in tiles:
+            if t:
+                t.draw()
+
     def __repr__(self):
-        # Flawed due to empty tiles. Rehaul needed. Sort and calculate gaps to be filled with Xs?
-        chars = [t.colour[0] if t else "X" for t in self.tiles]
-        rows = ["".join(chars[k:k + 7]) for k in range(0, 70, 7)]
-        return "-" * 7 + "\n" + "\n".join(rows) + "\n" + "-" * 7
+        out = "#|--0-|--1-|--2-|--3-|--4-|--5-|--6-"
+        for r in range(10):
+            out += "\n" + str(r) + "|" + "|".join(map(str, self.get_row(r)))
+        return out
 
 
 class Tile:
@@ -44,10 +51,26 @@ class Tile:
     def set_index(self):
         self.index = self.row * 7 + self.col
 
-    def __repr__(self):
-        return f"<{self.colour}>"
+    def draw(self):
+        colours = {'red': (0, 0, 255), 'green': (0, 255, 0), 'orange': (0, 172, 255), 'pink': (172, 0, 255),
+                   'violet': (255, 0, 172)}
+        x, y = self.ix, self.iy
+        if self.pip:
+            w, h = 66, 66
+            cv2.circle(img_rgb, (int(x + w / 2), int(y + h / 2)), int(min(w, h) / 2), colours[self.colour], -1)
+            cv2.putText(img_rgb, str(self.index), (x + 14, y + 42), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
+        else:
+            w, h = 92, 65
+            cv2.rectangle(img_rgb, (x, y), (x + w, y + h), colours[self.colour], -1)
+            cv2.putText(img_rgb, str(self.index), (x + 25, y + 40), cv2.FONT_HERSHEY_COMPLEX, 1,
+                        (255, 255, 255))
 
-tiles = []
+    def __str__(self):
+        return f"_{self.colour[:3]}"
+
+    def __repr__(self):
+        return str(self)
+
 
 screenshot_num = 12
 
@@ -56,6 +79,7 @@ full_img_rgb = cv2.imread('..\Images\Screenshots\Screenshot ' + str(screenshot_n
 img_rgb = full_img_rgb[180:180 + 890, 580:580 + 690]
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
+tiles = []
 # BGR
 colours = {'red': (0, 0, 255), 'green': (0, 255, 0), 'orange': (0, 172, 255), 'pink': (172, 0, 255),
            'violet': (255, 0, 172)}
@@ -65,8 +89,8 @@ for col in colours:
             template = cv2.imread(f"../Images/Templates/{col}-pip.jpg", 0)
         else:
             template = cv2.imread(f"../Images/Templates/{col}.jpg", 0)
-        w, h = template.shape[::-1]
 
+        w, h = template.shape[::-1]
         res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
         threshold = 0.92
         loc = np.where(res >= threshold)
@@ -79,23 +103,12 @@ for col in colours:
                     break
             else:  # I'm so happy that I used this! :)
                 tiles.append(Tile(_ix=pt[0], _iy=pt[1], _colour=col, _pip=pip))
-                # TODO - Move this into Tile class? Tile.draw()?
-                if not pip:
-                    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), colours[col], -1)
-                else:
-                    cv2.circle(img_rgb, (int(pt[0] + w / 2), int(pt[1] + h / 2)), int(min(w, h)/2), colours[col], -1)
 
-# Set row of tiles ('normalise' y by subtracting minimum.)
-min_y = min([t.iy for t in tiles])
-for t in tiles:
-    t.row = int(round((t.iy - min_y) / 100))
-    t.set_index()
-    cv2.putText(img_rgb, (" " + str(t.index))[1:], (t.ix + 25, t.iy + 40), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
 
-tiles.sort(key=lambda x: x.index)
 griddy = Grid(tiles)
-print([t for t in griddy.get_column(2)])
-#print(griddy)
+griddy.draw()
+
+print(griddy)
 
 cv2.imshow('res.png', img_rgb)
 cv2.waitKey()
